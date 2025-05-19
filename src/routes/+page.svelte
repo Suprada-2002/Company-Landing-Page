1dd2b89cb295faa88a5831545fc0c5f3445cfad4
Suprada Tripathy <87217681+Suprada-2002@users.noreply.github.com>
2025-05-19 20:50:37 +0530

    Update +page.svelte

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -3,11 +3,12 @@
     import Hero from "$lib/components/Hero.svelte";
     import Pricing from "$lib/components/Pricing.svelte";
     import Testimonials from "$lib/components/Testimonials.svelte";
-
+     import Workflow from "$lib/components/Workflow.svelte";
 </script>
 <div class="max-w-7xl mx-auto pt-20 px-6">
     <Hero />
     <Feature />
+    <Workflow />
     <Pricing />
     <Testimonials />
 </div>
