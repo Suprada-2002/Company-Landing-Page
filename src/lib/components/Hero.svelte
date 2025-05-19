3703b7f9808d49b6462e7611472ed7b609d16bed
Suprada Tripathy <87217681+Suprada-2002@users.noreply.github.com>
2025-05-19 19:05:48 +0530

    adding Hero.svelte

---
+++ src/lib/components/Hero.svelte
@@ -0,0 +1,24 @@
+<div class="flex flex-col items-center mt-6 lg:mt-20">
+    <h1 class="text-4xl sm:text-6xl lg:text-7xl text-center tracking-wide">VirtualR build tools
+    <span class="bg-gradient-to-r from-orange-500 to-red-800 text-transparent bg-clip-text">
+        for developers
+    </span>
+    </h1>
+
+    <p class="mt-10 text-lg text-center text-neutral-500 max-w-4xl">
+        Empower your creativity and bring your VR app ideas to life with our
+        intuitive development tools. Get started today and turn your imagination
+        into immersive reality!
+    </p>
+
+    <div>
+        <a>Start for free</a>
+        <a>Documentation</a>
+    </div>
+
+    <div>
+        <span>video</span>
+        <span>video</span>
+    </div>
+
+</div>
